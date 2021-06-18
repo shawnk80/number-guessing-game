b626753757e3a8b845336d17750bf183c1f87868
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-06-18 15:49:51 -0500

    Shawn's guessing game

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
-console.log("HELLO OSCAR PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+let myRandomNumber = Math.floor(Math.random() * 100);
+console.log(myRandomNumber)
+
+const checkGuess = () => {
+  yourGuess = document.getElementById("input-guess").value;
+  guessResult = document.getElementById("guess-result");
+  previousGuesses = document.getElementById("previous-guess");
+
+  if (!yourGuess || !guessResult || !previousGuesses)
+    return;
+
+  if (yourGuess > myRandomNumber) {
+    guessResult.innerHTML = "Too High"
+    guess = document.createElement("p")
+    guess.innerHTML = yourGuess
+    previousGuesses.appendChild(guess)
+  }
+  else if (yourGuess < myRandomNumber) {
+    guessResult.innerHTML = "Too Low"
+    guess = document.createElement("p")
+    guess.innerHTML = yourGuess
+    previousGuesses.appendChild(guess)
+  }
+  else {
+    guessResult.innerHTML = "Right on!!"
+  }
+
+}
